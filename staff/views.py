e9b231f57d7c0237bcd24abb70d69c51a8a7b032
ale <criabiobr@gmail.com>
2023-10-08 11:51:15 +0100

    feed back messages in create user

--- staff/views.py
+++ staff/views.py
@@ -2,8 +2,8 @@ import uuid
 
 from django.shortcuts import render
 from django.views import View
-
 from django.conf import settings
+from django.contrib import messages
 from core.models import CustomUser
 from core.email import send_email
 from staff.forms import UserRegister
@@ -21,19 +21,26 @@ class Home(View):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if not form.is_valid():
-            return self._render_login_form(request, form)
+            message = 'invalid form'
+            return self._render_login_form(request, form, message=message)
 
         new_user = self._register_user(request)
+
+        if new_user is None:
+            message = 'Problems to save user'
+            return self._render_login_form(request, form, message=message)
 
         send_email(
             f'Hello {new_user.first_name}',
             f'your password, {new_user.password}',
             new_user.email
         )
-        return self._render_login_form(request, form)
+        message = 'User created successfully.'
+        return self._render_login_form(request, form, message=message)
 
-    def _render_login_form(self, request, form):
-        return render(request, self.template_index, {'form': form})
+    def _render_login_form(self, request, form, message = None):
+        context = {'form': form, 'message': message}
+        return render(request, self.template_index, context)
 
     def _register_user(self, request):
         email = request.POST.get('email')
@@ -48,8 +55,8 @@ class Home(View):
         elif role == CustomUser.STUDENT:
             new_user = Student(email=email, first_name=first_name, role=role)
         else:
-            raise ValueError("Invalid role")
+            return None
 
         new_user.set_password(password)
-        new_user.save()
+        # new_user.save()
         return new_user
